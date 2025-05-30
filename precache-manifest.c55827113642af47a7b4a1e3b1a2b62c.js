c3a35eee58d9ec48d1dede676ff693bcc54b3c05
jaya pulicherla <36218160+jayasamhitha-pulicherla-au13@users.noreply.github.com>
2025-05-29 21:42:51 -0500

    Updates

---
+++ precache-manifest.c55827113642af47a7b4a1e3b1a2b62c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10bd31c9cd8d5b64e9a0a06b7bf28670",
+    "url": "/jayapulicherla-dev/my_portfolio/index.html"
+  },
+  {
+    "revision": "4a0936fe5aa5c557a523",
+    "url": "/jayapulicherla-dev/my_portfolio/static/css/main.85e4936f.chunk.css"
+  },
+  {
+    "revision": "733ffb65074bd3cdceb0",
+    "url": "/jayapulicherla-dev/my_portfolio/static/js/2.065621da.chunk.js"
+  },
+  {
+    "revision": "4a0936fe5aa5c557a523",
+    "url": "/jayapulicherla-dev/my_portfolio/static/js/main.8cc0bd1a.chunk.js"
+  },
+  {
+    "revision": "d6a9e0f1655e9d300ca1",
+    "url": "/jayapulicherla-dev/my_portfolio/static/js/runtime~main.971bca7f.js"
+  }
+]);
